28febd45281045f4678b0ef0446c57fbbfbc22a1
Petryck William <petryck@i9devgroup.com>
2024-02-05 17:13:30 -0300

    .
    .

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 const server = http.createServer(app); 
 
 // Statics
-app.use('/', express.static(path.join(__dirname, '../public')))
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 // Routes
 app.use('/api', listApi);

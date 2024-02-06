92762aae174ce01667df6b1a7488d2929051905c
Petryck William <petryck@i9devgroup.com>
2024-02-06 08:53:11 -0300

    .
    .

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ io.on('connection', (socket) => {
 
 
 // connection
-const port = process.env.PORT || 9005;
+const port = process.env.PORT || 6666;
 server.listen(port, () =>
   console.log(`Listening to port http://localhost:${port} Node.js v${process.versions.node}!`)
 );

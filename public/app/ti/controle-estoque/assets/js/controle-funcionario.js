6b36c7428711ec4dbff4f5b86d4362c6b0a8b646
Day-Alves <dayana.alves@conlinebr.com.br>
2024-06-18 15:00:46 -0300

    Renome

--- public/app/ti/controle-estoque/assets/js/controle-funcionario.js
+++
@@ -1,6 +0,0 @@
-// Esta função é executada após toda a página ser executada
-document.addEventListener("DOMContentLoaded", async () => {
-    eventoClick()
-    checkInputs() //Funçao que verifica se os inputs não estão vazios
-
-})

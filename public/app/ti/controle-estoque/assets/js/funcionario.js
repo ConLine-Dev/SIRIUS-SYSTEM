6f2bd8f0e0a1392e46e5b66e9b52ef7b1525edc1
Day-Alves <dayana.alves@conlinebr.com.br>
2024-06-18 15:01:02 -0300

    Criado um novo nome para  a pasta

---
+++ public/app/ti/controle-estoque/assets/js/funcionario.js
@@ -0,0 +1,6 @@
+// Esta função é executada após toda a página ser executada
+document.addEventListener("DOMContentLoaded", async () => {
+    eventoClick()
+    checkInputs() //Funçao que verifica se os inputs não estão vazios
+
+})

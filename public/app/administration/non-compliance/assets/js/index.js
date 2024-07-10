0f47d05f3c36634c1d9ce8406ad15862ad5c889b
Petryck William <petryck@i9devgroup.com>
2024-07-10 17:42:21 -0300

    .
    .

--- public/app/administration/non-compliance/assets/js/index.js
+++ public/app/administration/non-compliance/assets/js/index.js
@@ -4,19 +4,20 @@ const elements = {
     rowTableOccurence: document.querySelectorAll('#occurrences_table tbody tr')
 }
 
+
 async function listPendingOccurrences(){
     // Fazer a requisição à API
     const dados = await makeRequest(`/api/non-compliance/getPendingOccurrences`);
 
     // Destruir a tabela existente, se houver
-    if ($.fn.DataTable.isDataTable('#occurrences_table')) {
-        $('#occurrences_table').DataTable().destroy();
+    if ($.fn.DataTable.isDataTable('#pending_occurrences_table')) {
+        $('#pending_occurrences_table').DataTable().destroy();
     }
 
     // Criar a nova tabela com os dados da API
-    $('#occurrences_table').DataTable({
-        dom: 'Bfrtip',
-        pageLength: 10,
+    $('#pending_occurrences_table').DataTable({
+        dom: 'frtip',
+        pageLength: 5,
         order: [[0, 'desc']],
         data: dados,
         pageInfo: false,
@@ -41,11 +42,60 @@ async function listPendingOccurrences(){
         },
         "rowCallback": function(row, data, index) {
             // Adiciona um atributo id a cada linha
-            $(row).attr('ocurrence-id', data.id);
+            $(row).attr('occurrence-id', data.id);
         },
         initComplete: function () {
             requestAnimationFrame(async () => {
-                await dblClickOnOccurrence()
+                await dblClickOnOccurrence('#pending_occurrences_table')
+            });
+        },
+    });
+
+    
+}
+
+async function listAllOccurrences(){
+    // Fazer a requisição à API
+    const dados = await makeRequest(`/api/non-compliance/AllOccurrence`);
+
+    // Destruir a tabela existente, se houver
+    if ($.fn.DataTable.isDataTable('#all_occurrences_table')) {
+        $('#all_occurrences_table').DataTable().destroy();
+    }
+
+    // Criar a nova tabela com os dados da API
+    $('#all_occurrences_table').DataTable({
+        dom: 'frtip',
+        pageLength: 5,
+        order: [[0, 'desc']],
+        data: dados,
+        pageInfo: false,
+        bInfo: false,
+        columns: [
+            { data: 'reference' },
+            { data: 'title' },
+            { data: 'description' },
+            { data: 'type' },
+            { data: 'responsibles' },
+            { data: 'status' },
+            { data: 'date_occurrence' },
+            // { data: 'action' }
+            // Adicione mais colunas conforme necessário
+        ],
+        buttons: [
+            'excel', 'pdf'
+        ],
+        language: {
+            url: "https://cdn.datatables.net/plug-ins/1.12.1/i18n/pt-BR.json",
+            searchPlaceholder: 'Pesquisar...',
+        },
+        "rowCallback": function(row, data, index) {
+            // Adiciona um atributo id a cada linha
+            $(row).attr('occurrence-id', data.id);
+        },
+        initComplete: function () {
+            requestAnimationFrame(async () => {
+                await dblClickOnOccurrence('#all_occurrences_table')
             });
         },
     });
@@ -74,29 +124,34 @@ async function clickNewOccurence(){
 
 }
 
-async function dblClickOnOccurrence(){
-    const rowTableOccurence = document.querySelectorAll('#occurrences_table tbody tr')
+async function dblClickOnOccurrence(tableId){
+    const rowTableOccurence = document.querySelectorAll(`${tableId} tbody tr`);
 
     for (let index = 0; index < rowTableOccurence.length; index++) {
         const element = rowTableOccurence[index];
 
-        element.addEventListener('dblclick', async function(e){
-            e.preventDefault()
-            const id = this.getAttribute('ocurrence-id')
-
+        // Define a função de callback do evento
+        const handleDoubleClick = async function(e) {
+            e.preventDefault();
+            const id = this.getAttribute('occurrence-id');
             const body = {
                 url: `/app/administration/non-compliance/view-occurrence?id=${id}`
-            }
-    
+            };
+
             window.ipcRenderer.invoke('open-exWindow', body);
-        })
-        
+        };
+
+        // Remove event listener se já existir
+        element.removeEventListener('dblclick', handleDoubleClick);
+        // Adiciona event listener
+        element.addEventListener('dblclick', handleDoubleClick);
     }
 }
 
 window.addEventListener("load", async () => {
   
     await listPendingOccurrences();
+    await listAllOccurrences();
     await Events()
 
 

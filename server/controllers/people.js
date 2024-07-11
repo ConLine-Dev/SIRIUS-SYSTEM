5ee2b4a59026b2682c5f7cb522307a1ad242d31a
Ederson Arruda Ceccato <edersonceccato@gmail.com>
2024-07-11 09:02:18 -0300

    Alterado rota de listar pessoas para receber alguns parametros e criado rota para para pegar a todas as categorias

--- server/controllers/people.js
+++ server/controllers/people.js
@@ -1,10 +1,23 @@
 const { executeQuery } = require('../connect/mysql');
 
 const People = {
-   getAllPeople: async function(){
+   getAllPeople: async function(peopleCategorySelected, peopleAllType){
+      const peopleType = peopleAllType ? `type_people IN ${peopleAllType}` : 'type_people IN (0,1)';
+      const peopleCategory = peopleCategorySelected ? `AND people_category_id IN ${peopleCategorySelected}` : '';
+      let result = await executeQuery(`
+         SELECT 
+            *
+         FROM 
+            people
+         WHERE
+            ${peopleType}
+            ${peopleCategory}
+         ORDER BY fantasy_name ASC`);
+      return result;
+   },
 
-      let result = await executeQuery(`SELECT * FROM people ORDER BY name ASC`);
-   
+   getAllPeopleCategory: async function() {
+      let result = await executeQuery(`SELECT * FROM people_category ORDER BY name ASC`)
       return result;
    }
 }
--- server/routes/api-people.js
+++ server/routes/api-people.js
@@ -4,10 +4,21 @@ const path = require("path");
 const fs = require('fs');
 const { People } = require('../controllers/people');
 
-
 router.post('/listAllPeople', async (req, res, next) => {
+    const {peopleCategorySelected, peopleAllType} = req.body;
     try {
-        const result = await People.getAllCollaborators();
+        const result = await People.getAllPeople(peopleCategorySelected, peopleAllType);
+
+        res.status(200).json(result)
+    } catch (error) {
+
+        res.status(404).json('Erro')
+    }
+});
+
+router.post('/getAllPeopleCategory', async (req, res, next) => {
+    try {
+        const result = await People.getAllPeopleCategory();
 
         res.status(200).json(result)
     } catch (error) {

258ab5d86df82e6dbd902add1d803bb1d762eeb6
Ederson Arruda Ceccato <edersonceccato@gmail.com>
2024-08-12 10:30:03 -0300

    Criado funcao de verificar se o usuario esta logado ou nao para dar acesso ah pagina

--- public/app/people/all-people/assets/js/script.js
+++ public/app/people/all-people/assets/js/script.js
@@ -1,3 +1,68 @@
+// Função para verificar se o tempo de login expirou
+async function checkLoginExpiration() {
+    // Verifica o localStorage para alterar a mensagem de boas vindas
+    const StorageGoogleData = localStorage.getItem('StorageGoogle');
+    const StorageGoogle = JSON.parse(StorageGoogleData);
+ 
+    if (!localStorage.getItem('StorageGoogle')) {
+        window.location.href = '/app/login';
+    } else {
+        document.querySelector('body').style.display = 'block'
+    }
+ 
+ 
+ 
+    const loginTime = localStorage.getItem('loginTime');
+ 
+    if (loginTime) {
+        const currentTime = new Date().getTime();
+        const elapsedTime = currentTime - parseInt(loginTime);
+    
+        // 24 horas em milissegundos
+        const twentyFourHours = 1000;
+ 
+        if (elapsedTime >= twentyFourHours) {
+            // Limpa os dados do usuário e redireciona para a página de login
+            localStorage.removeItem('StorageGoogle');
+            localStorage.removeItem('loginTime');
+            window.location.href = '/app/login';
+        }
+    }
+}
+
+// Verifica o localStorage para setar informações
+async function getInfosLogin() {
+    const StorageGoogleData = localStorage.getItem('StorageGoogle');
+    const StorageGoogle = JSON.parse(StorageGoogleData);
+ 
+    return StorageGoogle;
+}
+ 
+async function setInfosLogin(StorageGoogle) {
+    document.querySelectorAll('.imgUser').forEach(element => {
+        element.src = StorageGoogle.picture ? StorageGoogle.picture : StorageGoogle.system_image
+    });
+ 
+    document.querySelectorAll('.UserName').forEach(element => {
+        element.textContent = StorageGoogle.given_name.replace(/[^a-zA-Z\s]/g, '');
+    });
+ 
+    document.querySelectorAll('.buttonLogout').forEach(element => {
+        element.addEventListener('click', function (e) {
+                e.preventDefault()
+        
+                localStorage.removeItem('StorageGoogle');
+                localStorage.removeItem('loginTime');
+        
+                window.location.href = '/app/login'
+        })
+ 
+    });
+ 
+ 
+}
+
+
 let startDateGlobal, endDateGlobal;
 
 const socket = io();
@@ -465,22 +530,31 @@ async function createPeople() {
 
 // Função executada após toda a página ser executada
 window.addEventListener("load", async () => {
-   const getAllPeople = await makeRequest('/api/people/listAllPeople', 'POST',);
-   const getAllPeopleCategory = await makeRequest('/api/people/getAllPeopleCategory', 'POST',);
-   const getAllPeopleStatus = await makeRequest('/api/people/getAllPeopleStatus', 'POST',);
-   const getAllCommercial = await makeRequest('/api/people/getAllCommercial', 'POST',);
-   const getAllCollaboratorsResponsable = await makeRequest('/api/people/getAllCollaboratorsResponsable', 'POST',);
+    await checkLoginExpiration()
 
-   await listPeople(getAllPeople);
-   await createSelectPeopleCategory(getAllPeopleCategory);
-   await createSelectPeopleStatus(getAllPeopleStatus);
-   await createSelectCommercial(getAllCommercial);
-   await createSelectCollaboratorResponsable(getAllCollaboratorsResponsable);
-   await eventClick();
-   await initializeDatePicker();
-   await active_tooltip();
+    setInterval(async () => {
+        await checkLoginExpiration()
+    }, 1000);
 
-   // Tela de carregando 'add=quando vc fecha algo/remove=quando vc abre algo'
-   document.querySelector('#loader2').classList.add('d-none')
+    const StorageGoogle = await getInfosLogin();
+    await setInfosLogin(StorageGoogle)
+    
+    const getAllPeople = await makeRequest('/api/people/listAllPeople', 'POST',);
+    const getAllPeopleCategory = await makeRequest('/api/people/getAllPeopleCategory', 'POST',);
+    const getAllPeopleStatus = await makeRequest('/api/people/getAllPeopleStatus', 'POST',);
+    const getAllCommercial = await makeRequest('/api/people/getAllCommercial', 'POST',);
+    const getAllCollaboratorsResponsable = await makeRequest('/api/people/getAllCollaboratorsResponsable', 'POST',);
+
+    await listPeople(getAllPeople);
+    await createSelectPeopleCategory(getAllPeopleCategory);
+    await createSelectPeopleStatus(getAllPeopleStatus);
+    await createSelectCommercial(getAllCommercial);
+    await createSelectCollaboratorResponsable(getAllCollaboratorsResponsable);
+    await eventClick();
+    await initializeDatePicker();
+    await active_tooltip();
+
+    // Tela de carregando 'add=quando vc fecha algo/remove=quando vc abre algo'
+    document.querySelector('#loader2').classList.add('d-none')
 
 })
--- public/app/people/create-people/assets/js/script.js
+++ public/app/people/create-people/assets/js/script.js
@@ -1,3 +1,67 @@
+// Função para verificar se o tempo de login expirou
+async function checkLoginExpiration() {
+   // Verifica o localStorage para alterar a mensagem de boas vindas
+   const StorageGoogleData = localStorage.getItem('StorageGoogle');
+   const StorageGoogle = JSON.parse(StorageGoogleData);
+
+   if (!localStorage.getItem('StorageGoogle')) {
+       window.location.href = '/app/login';
+   } else {
+       document.querySelector('body').style.display = 'block'
+   }
+
+
+
+   const loginTime = localStorage.getItem('loginTime');
+
+   if (loginTime) {
+       const currentTime = new Date().getTime();
+       const elapsedTime = currentTime - parseInt(loginTime);
+   
+       // 24 horas em milissegundos
+       const twentyFourHours = 1000;
+
+       if (elapsedTime >= twentyFourHours) {
+           // Limpa os dados do usuário e redireciona para a página de login
+           localStorage.removeItem('StorageGoogle');
+           localStorage.removeItem('loginTime');
+           window.location.href = '/app/login';
+       }
+   }
+}
+
+// Verifica o localStorage para setar informações
+async function getInfosLogin() {
+   const StorageGoogleData = localStorage.getItem('StorageGoogle');
+   const StorageGoogle = JSON.parse(StorageGoogleData);
+
+   return StorageGoogle;
+}
+
+async function setInfosLogin(StorageGoogle) {
+   document.querySelectorAll('.imgUser').forEach(element => {
+       element.src = StorageGoogle.picture ? StorageGoogle.picture : StorageGoogle.system_image
+   });
+
+   document.querySelectorAll('.UserName').forEach(element => {
+       element.textContent = StorageGoogle.given_name.replace(/[^a-zA-Z\s]/g, '');
+   });
+
+   document.querySelectorAll('.buttonLogout').forEach(element => {
+       element.addEventListener('click', function (e) {
+               e.preventDefault()
+       
+               localStorage.removeItem('StorageGoogle');
+               localStorage.removeItem('loginTime');
+       
+               window.location.href = '/app/login'
+       })
+
+   });
+
+
+}
+
 // Função para formatar o CNPJ e CPF do input
 function formatCnpjCpfInput(value) {
    // Remove todos os caracteres não numéricos
@@ -813,6 +877,12 @@ async function eventClick() {
 
 // Função executada após toda a página ser executada
 window.addEventListener("load", async () => {
+   await checkLoginExpiration()
+
+   setInterval(async () => {
+       await checkLoginExpiration()
+   }, 1000);
+
    await createSelectPeopleType();
    await createSelectWithChoices()
    await inputCepVerification();

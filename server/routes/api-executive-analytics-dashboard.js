09e2ba18c950c8c4c664a35b4e3354c750b1b9a5
Lucas Santos Peres <luc_peres@hotmail.com>
2024-08-22 15:22:43 -0300

    add + update /executive-analytics-dashboard

--- server/routes/api-executive-analytics-dashboard.js
+++ server/routes/api-executive-analytics-dashboard.js
@@ -2,13 +2,48 @@ const express = require('express');
 const router = express.Router();
 const path = require("path");
 const fs = require('fs');
-const { Exec_Analytics } = require('../controllers/executive-analytics-dashboard.js');
+const { executiveAnalytics } = require('../controllers/executive-analytics-dashboard.js');
 
 module.exports = function(io) {
    // Lista todos os colaboradores;
-   router.post('/getAllCollaborator', async (req, res, next) => {
+   router.post('/totalOffers', async (req, res, next) => {
+      const data = req.body;
       try {
-         const result = await Exec_Analytics.getAllCollaborator();
+         const result = await executiveAnalytics.totalOffers(data);
+
+         res.status(200).json(result)
+      } catch (error) {
+
+         res.status(404).json('Erro')
+      }
+   });
+
+   router.get('/countOffers', async (req, res, next) => {
+      try {
+         const result = await executiveAnalytics.countOffers();
+
+         res.status(200).json(result)
+      } catch (error) {
+
+         res.status(404).json('Erro')
+      }
+   });
+
+   router.get('/countProcesses', async (req, res, next) => {
+      try {
+         const result = await executiveAnalytics.countProcesses();
+
+         res.status(200).json(result)
+      } catch (error) {
+
+         res.status(404).json('Erro')
+      }
+   });
+
+   router.post('/totalProcesses', async (req, res, next) => {
+      const data = req.body;
+      try {
+         const result = await executiveAnalytics.totalProcesses(data);
 
          res.status(200).json(result)
       } catch (error) {

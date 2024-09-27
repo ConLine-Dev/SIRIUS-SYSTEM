1d3b4584c14ceb0ffef4dc363b285ba9c44864d7
Ederson Arruda Ceccato <edersonceccato@gmail.com>
2024-09-27 16:38:20 -0300

    Criado tela de nova categoria

---
+++ public/app/stock/new-category/assets/js/index.js
@@ -0,0 +1,6 @@
+// Função executada após toda a página ser executada
+window.addEventListener("load", async () => {
+
+   // Tela de carregando 'add=quando vc fecha algo/remove=quando vc abre algo'
+   document.querySelector('#loader2').classList.add('d-none')
+})

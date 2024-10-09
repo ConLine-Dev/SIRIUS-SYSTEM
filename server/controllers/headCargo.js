f8f415bada3e3970b23473cfd28689b3b7b73cd9
Petryck William <petryck@i9devgroup.com>
2024-10-09 15:14:03 -0300

    Correção: Alterar condição de pagamento quitado para fatura quitada
    Alterar a condição de pagamento quitado para verificar o status da fatura em vez da situação da comissão interna. Isso garantirá que apenas as faturas realmente quitadas sejam consideradas quitadas.

--- server/controllers/headCargo.js
+++ server/controllers/headCargo.js
@@ -161,7 +161,7 @@ const headcargo = {
                WHEN Lhs.Situacao_Pagamento = 0 THEN 0
                WHEN Lhs.Situacao_Pagamento = 1 THEN 1
                WHEN Lhs.Situacao_Pagamento = 2 THEN 2
-               WHEN Lhs.Situacao_Pagamento = 3 AND (ComInter.Situacao = 2 OR ComInter.Situacao IS NULL) THEN 3
+               WHEN Lhs.Situacao_Pagamento = 3 AND Fde.Status_Fatura = 'Quitado' THEN 3
             END AS PagamentoCodigo,
       
             Lhs.Data_Pagamento_Local AS Data_Pagamento,

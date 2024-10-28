320c720334b68b776dc8b037d3582f8c3e5c7030
Petryck William <petryck@i9devgroup.com>
2024-10-28 18:05:17 -0300

    .

---
+++ public/app/ti/data-security-hub/assets/js/index.js
@@ -0,0 +1,15 @@
+const table = [];
+
+
+// Esta função é executada quando o documento HTML é completamente carregado e analisado
+document.addEventListener("DOMContentLoaded", async () => {
+
+    //Chama a função
+    // await generateTable()
+
+    const socket = io();
+
+  
+    document.querySelector('#loader2').classList.add('d-none')
+
+})

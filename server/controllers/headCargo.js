2e67e3564930bd8314878c6c6de0ee8c2500e98a
Petryck William <petryck@i9devgroup.com>
2024-11-05 08:30:03 -0300

    Refatoração: Verificação de último envio de e-mail para data nula
    Verifica se o campo last_sent_date é nulo antes de converter para Date, evitando erros de conversão.

--- server/controllers/headCargo.js
+++ server/controllers/headCargo.js
@@ -2304,7 +2304,7 @@ LEFT OUTER JOIN
          // Verifica se ja foi enviado um email hoje
          const today = new Date().toISOString().split('T')[0]; // Pega a data atual no formato YYYY-MM-DD
          const emailLog = await executeQuery("SELECT last_sent_date FROM user_sessions_email_log_error WHERE email_type = 'token_error'")
-         const lastSendDate = new Date(emailLog.last_sent_date).toISOString().split('T')[0];
+         const lastSendDate = emailLog.last_sent_date ? new Date(emailLog.last_sent_date).toISOString().split('T')[0] : null;
 
          if (!emailLog || lastSendDate !== today) {
             let messageBody = `

55a03819494d199687bb2ce599b134d7df4d8f57
Day-Alves <dayana.alves@conlinebr.com.br>
2024-11-18 15:31:40 -0300

    Criado rota na api para listar os colaboradores

--- server/controllers/rh-payroll.js
+++ server/controllers/rh-payroll.js
@@ -1,13 +1,32 @@
-const { executeQuerySQL } = require('../connect/sqlServer');
 const { executeQuery } = require('../connect/mysql');
 
 const rhPayroll = {
 
-
-
+    // Função para obter módulos de um usuário agrupados por categoria
+    getUserModulesByCategory: async function(userId) {
+        const result = await executeQuery(`
+            SELECT 
+                mc.id AS category_id,
+                mc.name AS category_name,
+                m.id AS module_id,
+                m.title AS module_title,
+                IF(uma.user_id IS NULL, 0, 1) AS has_access
+            FROM 
+                module_categories mc
+            LEFT JOIN 
+                module_category_relations mcr ON mc.id = mcr.category_id
+            LEFT JOIN 
+                modules m ON mcr.module_id = m.id
+            LEFT JOIN 
+                modules_acess uma ON m.id = uma.modules_id AND uma.user_id = ${userId}
+            ORDER BY 
+                mc.name, m.title
+        `);
+        return result;
+    }
 }
 
-
+// Exporta o objeto moduleManagement para uso em outros módulos
 module.exports = {
     rhPayroll,
- };
+};
--- server/routes/api-rh-payroll.js
+++ server/routes/api-rh-payroll.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const path = require("path");
-const fs = require('fs');
-const { rhPayroll } = require('../controllerrh-payroll.js');
+const { rhPayroll } = require('../controllers/rh-payroll');
 
 module.exports = function(io) {
- }
+
+    // Rota para obter módulos de um usuário agrupados por categoria
+    router.get('/getUserModulesByCategory/:userId', async (req, res, next) => {
+        try {
+            const userId = req.params.userId;
+            const result = await rhPayroll.getUserModulesByCategory(userId);
+            res.status(200).json(result); // 200 OK
+        } catch (error) {
+            console.log(error)
+            res.status(400).json(error); // 400 Bad Request
+        }
+    });
+
+     // Retorna o router configurado
+     return router;
+    
+}

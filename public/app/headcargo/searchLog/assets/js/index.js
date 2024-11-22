7349b6aa5c74dcbf610e464d77204240d7fb3863
Ederson Arruda Ceccato <edersonceccato@gmail.com>
2024-11-22 12:05:33 -0300

    Ajustado front e js para receber os dados das rotas headcargo

--- public/app/headcargo/searchLog/assets/js/index.js
+++ public/app/headcargo/searchLog/assets/js/index.js
@@ -1,46 +1,22 @@
-/**
- * Desenvolvido por: Petryck William
- * GitHub: https://github.com/peewilliam
- */
-
-/**
- * Verifica o localStorage para alterar a mensagem de boas vindas
- */
+// Verifica o localStorage para alterar a mensagem de boas vindas
 // Obtém os dados armazenados no localStorage sob a chave 'StorageGoogle'
 const StorageGoogleData = localStorage.getItem('StorageGoogle');
 // Converte os dados armazenados de JSON para um objeto JavaScript
 const StorageGoogle = JSON.parse(StorageGoogleData);
 
-// Variável para contar o número de toasts exibidos
-let toastCount = 0, verifyGlobal = true;
+// Função para submeter aos logs do head.
+let tables;
+async function submitLog() {
+    document.querySelector('#loader').classList.remove('d-none'); // Exibe o loader
 
-/**
- * Evento que será disparado quando o DOM estiver completamente carregado,
- * mas antes que recursos adicionais (como imagens e folhas de estilo) sejam carregados.
- */
-document.addEventListener("DOMContentLoaded", async () => {
-    // Esconder o loader
-    document.querySelector('#loader2').classList.add('d-none');
-});
-
-
-
-
-
-
-
-/**
- * Função para submeter a comissão.
- */
-async function submitCommission() {
-    document.querySelector('#loader2').classList.remove('d-none'); // Exibe o loader
-
+    const selectedTable = await getSelectTable();
+    const selectedColumn = await getSelectColumn();
     
     const filters = {
         dataAte: document.querySelector('#dataAte').value,
         dataDe: document.querySelector('#dataDe').value,
-        tabela: document.querySelector('#tabela').value,
-        coluna: document.querySelector('#coluna').value,
+        tabela: selectedTable,
+        coluna: selectedColumn,
         valor: document.querySelector('#valor').value,
     }
 
@@ -48,10 +24,12 @@ async function submitCommission() {
     const dados = await makeRequest(`/api/headcargo/searchLog/filter`, 'POST', { filters });
     console.log(dados)
 
+    // Destruir a tabela existente, se houver
+    if ($.fn.DataTable.isDataTable('#table_result_log')) {
+        $('#table_result_log').DataTable().destroy();
+    }
 
-
-
-    tables['table_commission_commercial'] = $('#table_commission_commercial').DataTable({
+    $('#table_result_log').DataTable({
         paging: false,
         scrollX: true,
         scrollY: '60vh',
@@ -61,6 +39,7 @@ async function submitCommission() {
         data: dados, // Define os dados da tabela
         columns: [
             { data: 'Usuario' }, // Coluna de modal
+            { data: 'Data' }, // Coluna de importador
             { data: 'Data_Inicio_Transacao' }, // Coluna de processo
             { data: 'Data_Termino_Transacao' }, // Coluna de abertura
             { data: 'Tempo_Transacao' }, // Coluna de data de compensação
@@ -68,58 +47,175 @@ async function submitCommission() {
             { data: 'Computador' }, // Coluna de cliente
             { data: 'Tabela' }, // Coluna de vendedor
             { data: 'Tipo' }, // Coluna de inside
-            { data: 'Data' }, // Coluna de importador
             { data: 'Indice' }, // Coluna de exportador
             { data: 'Campos' }, // Coluna de comissão de vendedor
         ],
         language: {
-            url: "https://cdn.datatables.net/plug-ins/1.12.1/i18n/pt-BR.json",
+            url: "../../assets/libs/datatables/pt-br.json",
             searchPlaceholder: 'Pesquisar...',
             Search: '',
         }
     });
 
-    document.querySelector('#loader2').classList.add('d-none'); // Esconde o loader
+    document.querySelector('#loader').classList.add('d-none'); // Esconde o loader
+};
 
+let selectTable, selectColumn;
+async function createSelectWithChoices() {
+    // renderiza o select com as opções formatadas
+    selectTable = new Choices('#selectTable', {
+       allowHTML: true,
+       allowSearch: true,
+       shouldSort: false,
+       removeItemButton: true,
+       noChoicesText: 'Não há opções disponíveis',
+       noResultsText: 'Não há opções disponíveis',
+       position: 'bottom'
+    });
+
+    // renderiza o select com as opções formatadas
+    selectColumn = new Choices('#selectColumn', {
+        allowHTML: true,
+        allowSearch: true,
+        shouldSort: false,
+        removeItemButton: true,
+        noChoicesText: 'Não há opções disponíveis',
+        noResultsText: 'Não há opções disponíveis',
+        position: 'bottom'
+    });
+};
+
+// Função para carregar as opções do banco de dados
+async function loadSelectTable() {
+    // Mostra o indicador de carregamento
+    selectTable.setChoices([{
+       value: '',
+       label: 'Carregando...',
+       disabled: true
+    }], 'value', 'label', true);
+ 
+    try {
+        // Simula uma chamada para o banco de dados
+        const getTable = await makeRequest('/api/headcargo/searchLog/getTables', 'POST',);
+ 
+        // Formate o array para ser usado com o Choices.js
+        const options = getTable.map(element => ({
+            value: element.TABLE_NAME,
+            label: element.TABLE_NAME
+        }));
+ 
+        // Atualiza as opções do Choices.js
+        selectTable.clearChoices();
+        selectTable.setChoices(options, 'value', 'label', true);
+    } catch (error) {
+        // Lida com o erro se a chamada para o banco de dados falhar
+        console.error('Erro ao carregar opções:', error);
+        selectTable.clearChoices();
+        selectTable.setChoices([{
+            value: '',
+            label: 'Erro ao carregar opções',
+            disabled: true
+        }], 'value', 'label', true);
+    }
+ 
+};
+ 
+// Função para selecionar a tabela
+async function setSelectTableFromDB(value) {
+    selectTable.setChoiceByValue(value);
+};
+ 
+// Função para pegar a opção selecionada do Select Tabela
+async function getSelectTable() {
+if (selectTable) {
+    const values = selectTable.getValue(true);
+    if (values && values.length === 0) {
+        return undefined;
+    } else {
+        return values;
+    }
+} else {
+    return undefined;
 }
+};
+ 
+// Adiciona um evento para carregar as opções quando o dropdown do select é exibido
+document.querySelector('#selectTable').addEventListener('showDropdown', async function() {
+    const currentValue = selectTable.getValue(true);
+    if (!currentValue || currentValue.length === 0) {
+        await loadSelectTable();
+    }
+});
+
+// Função para carregar as opções do banco de dados
+async function loadSelectColumn() {
+    // Mostra o indicador de carregamento
+    selectColumn.setChoices([{
+       value: '',
+       label: 'Carregando...',
+       disabled: true
+    }], 'value', 'label', true);
+
+    const selectedTable = await getSelectTable();
+ 
+    try {
+        // Simula uma chamada para o banco de dados
+        const getColumn = await makeRequest('/api/headcargo/searchLog/getColumns', 'POST', {table: selectedTable});
+ 
+        // Formate o array para ser usado com o Choices.js
+        const options = getColumn.map(element => ({
+            value: element.column_name,
+            label: element.column_name
+        }));
+        
+ 
+        // Atualiza as opções do Choices.js
+        selectColumn.clearChoices();
+        selectColumn.setChoices(options, 'value', 'label', true);
+    } catch (error) {
+        // Lida com o erro se a chamada para o banco de dados falhar
+        console.error('Erro ao carregar opções:', error);
+        selectColumn.clearChoices();
+        selectColumn.setChoices([{
+            value: '',
+            label: 'Erro ao carregar opções',
+            disabled: true
+        }], 'value', 'label', true);
+    }
+ 
+};
+
+// Função para pegar a opção selecionada do Select Tabela
+async function getSelectColumn() {
+    if (selectColumn) {
+        const values = selectColumn.getValue(true);
+        if (values && values.length === 0) {
+            return undefined;
+        } else {
+            return values;
+        }
+    } else {
+        return undefined;
+    }
+};
+
+// Adiciona um evento para carregar as opções quando o dropdown do select é exibido
+document.querySelector('#selectColumn').addEventListener('showDropdown', async function() {
+    const currentValue = selectColumn.getValue(true);
+    if (currentValue || !currentValue || currentValue.length === 0) {
+        await loadSelectColumn();
+    }
+});
 
 /**
- * Função para criar um toast.
- * @param {string} title - Título do toast
- * @param {string} text - Texto do toast
+ * Evento que será disparado quando o DOM estiver completamente carregado,
+ * mas antes que recursos adicionais (como imagens e folhas de estilo) sejam carregados.
  */
-function createToast(title, text) {
-    toastCount++; // Incrementa o contador de toasts
-    const toast = document.createElement('div'); // Cria um novo elemento div para o toast
-    toast.className = 'toast align-items-center border-0'; // Define as classes do toast
-    toast.id = `toast-${toastCount}`; // Define o ID do toast
-    toast.role = 'alert'; // Define o papel do toast como alerta
-    toast.ariaLive = 'assertive'; // Define a propriedade aria-live como assertiva
-    toast.ariaAtomic = 'true'; // Define a propriedade aria-atomic como true
-    toast.dataset.bsDelay = '5000'; // Define o atraso do toast para 5 segundos
+window.addEventListener("load", async () => {
+    await createSelectWithChoices(); // Cria o choices
 
-    const toastHeader = document.createElement('div'); // Cria um novo elemento div para o cabeçalho do toast
-    toastHeader.className = 'toast-header text-bg-danger'; // Define as classes do cabeçalho do toast
-    toastHeader.innerHTML = `
-        <strong class="me-auto">${title}</strong>
-        <small>Agora mesmo</small>
-        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-    `; // Define o conteúdo HTML do cabeçalho do toast
+    await loadSelectTable(); // Cria as opções das tabelas no select
 
-    const toastBody = document.createElement('div'); // Cria um novo elemento div para o corpo do toast
-    toastBody.className = 'toast-body'; // Define a classe do corpo do toast
-    toastBody.innerText = text; // Define o texto do corpo do toast
-
-    toast.appendChild(toastHeader); // Adiciona o cabeçalho ao toast
-    toast.appendChild(toastBody); // Adiciona o corpo ao toast
-
-    const toastContainer = document.getElementById('toast-container'); // Seleciona o contêiner de toasts
-    toastContainer.appendChild(toast); // Adiciona o toast ao contêiner
-
-    const bsToast = new bootstrap.Toast(toast); // Inicializa o toast com o Bootstrap
-    bsToast.show(); // Exibe o toast
-
-    toast.addEventListener('hidden.bs.toast', function() {
-        toastContainer.removeChild(toast); // Remove o toast do DOM quando ele for ocultado
-    });
-}
+    // Esconder o loader
+    document.querySelector('#loader2').classList.add('d-none');
+});

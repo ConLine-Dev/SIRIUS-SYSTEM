6c2963dd6a8333c829b73046c2ed53ab5a3d31e9
Ederson Arruda Ceccato <edersonceccato@gmail.com>
2024-11-27 18:00:56 -0300

    Feito inserção dos processos após ser realizado a busca

--- public/app/operational/part-lot/assets/js/create.js
+++ public/app/operational/part-lot/assets/js/create.js
@@ -10,66 +10,83 @@ function openWindow(url, width, height) {
    window.open(url, target, features);
 };
 
-// Função para listar todos os colaboradores
-async function listUsers() {
-   const users = await makeRequest(`/api/users/listAllUsers`);
-   const divListUsers = document.querySelector('.listUsers');
-   const searchInput = document.querySelector('.searchInput');
-
-   divListUsers.innerHTML = '';
-   let userHTML = '';
-
-   for (let i = 0; i < users.length; i++) {
-       const item = users[i];
-
-       userHTML += `<li class="list-group-item" data_userid="${item.userID}" data-username="${item.username.toLowerCase()} ${item.familyName.toLowerCase()}">
-                       <div class="d-flex align-items-center justify-content-between flex-wrap">
-                           <div class="d-flex align-items-center gap-2">
-                               <div> <span class="avatar bg-light"> <img src="https://cdn.conlinebr.com.br/colaboradores/${item.id_headcargo}" alt=""> </span> </div>
-                               <div> 
-                                   <span class="d-block fw-normal">${item.username} ${item.familyName}<i class="bi bi-patch-check-fill text-secondary ms-2"></i></span>   
-                               </div>
-                           </div>
-                       </div>
-                   </li>`;
-   }
-
-   divListUsers.innerHTML = userHTML;
-
-   // Adiciona o evento de input para filtrar usuários
-   searchInput.addEventListener('input', function () {
-      const searchTerm = this.value.toLowerCase();
-      const listItems = divListUsers.querySelectorAll('.list-group-item');
-
-      listItems.forEach(item => {
-         const username = item.getAttribute('data-username');
-         if (username.includes(searchTerm)) {
-            item.style.display = ''; // Mostra o item
-         } else {
-            item.style.display = 'none'; // Esconde o item
-         }
-      });
-   });
-};
-
 // Função para exibir os detalhes abaixo dos descontos
 function toggleDetails(button) {
    const detailsRow = button.closest("tr").nextElementSibling;
    const icon = button.querySelector("i");
 
-   if (detailsRow.style.display === "none" || !detailsRow.style.display) {
-     detailsRow.style.display = "table-row"; // Exibe a linha de detalhes
-     icon.classList.remove("ri-eye-close-line"); // Alterna o ícone para "olho aberto"
-     icon.classList.add("ri-eye-line");
+   if (detailsRow && detailsRow.classList.contains('details-row')) {
+      if (detailsRow.style.display === "none" || !detailsRow.style.display) {
+         detailsRow.style.display = "table-row"; // Exibe a linha de detalhes
+         icon.classList.remove("ri-eye-close-line"); // Alterna o ícone para "olho aberto"
+         icon.classList.add("ri-eye-line");
+      } else {
+         detailsRow.style.display = "none"; // Oculta a linha de detalhes
+         icon.classList.remove("ri-eye-line"); // Alterna o ícone para "olho fechado"
+         icon.classList.add("ri-eye-close-line");
+      }
    } else {
-     detailsRow.style.display = "none"; // Oculta a linha de detalhes
-     icon.classList.remove("ri-eye-line"); // Alterna o ícone para "olho fechado"
-     icon.classList.add("ri-eye-close-line");
+      console.error("Linha de detalhes não encontrada.");
    }
 };
- 
+
+// Busca os processos pela referencia externa e insere na tela
+document.getElementById('searchProcess').addEventListener('click', async function () {
+   document.querySelector('#loader').classList.remove('d-none');
+
+   const tableControlProcess = document.getElementById('tableControlProcess'); // Div que vai inserir os processos dentro
+   const searchInput = document.getElementById('searchInput').value;
+   
+   tableControlProcess.innerHTML = '';
+   let processHTML = '';
+
+   const process = await makeRequest(`/api/part-lot/processByRef`, 'POST', { externalRef: searchInput});
+
+   for (let i = 0; i < process.length; i++) {
+      const item = process[i];      
+   
+      processHTML += `<tbody class="files-list">
+                        <tr data-process-id="${item.IdLogistica_House}" class="odd">
+                           <td style="width: 15px !important;">
+                              <button onclick="toggleDetails(this)" class="btn btn-icon btn-primary-transparent rounded-pill btn-wave waves-effect waves-light" title="Visualizar Detalhes">
+                                 <i class="ri-eye-close-line"></i>
+                              </button>
+                           </td>
+                           <td style="max-width: 130px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
+                              <div>
+                                 <span class="d-block fw-normal">Processo:</span> 
+                                 <span class="d-block fw-bold" style="max-width: 400px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">${item.Numero_Processo}</span> 
+                              </div>
+                           </td>
+                           <td style="max-width: 130px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
+                              <div>
+                                 <span class="d-block fw-normal">Quantidade Container(s): ${item.Qtd_Containers}</span> 
+                                 <span class="d-block fw-bold" style="max-width: 400px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">${item.Containers}</span> 
+                              </div>
+                           </td>
+                           <td style="max-width: 130px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
+                              <div>
+                                 <span class="d-block fw-normal">Quantidade HBL(s): ${item.Qtd_Conhecimentos}</span> 
+                                 <span class="d-block fw-bold" style="max-width: 400px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">${item.Conhecimentos}</span> 
+                              </div>
+                           </td>
+                        </tr>
+                        <tr class="details-row" style="display: none;">
+                           <td colspan="4">
+                              <div>
+                                 Detalhes adicionais do processo: ${item.Detalhes}
+                              </div>
+                           </td>
+                        </tr>
+                     </tbody>`;
+   };
+   
+   tableControlProcess.innerHTML = processHTML;
+
+   document.querySelector('#loader').classList.add('d-none');
+});
+
 document.addEventListener("DOMContentLoaded", async () => {
 
-   // await listUsers();
    document.querySelector('#loader2').classList.add('d-none');
 });

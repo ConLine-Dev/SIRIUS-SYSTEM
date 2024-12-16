94aad933cecad00502da7b5b7f82af75737801f7
Ederson Arruda Ceccato <edersonceccato@gmail.com>
2024-12-16 17:59:02 -0300

    Finalizado rotas de listar parte lote

--- server/routes/api-part-lot.js
+++ server/routes/api-part-lot.js
@@ -74,5 +74,46 @@ module.exports = function(io) {
       }
    });
 
+   // Lista todos os parte lotes que encontrar no Sirius
+   router.post('/listParteLoteById', async (req, res, next) => {
+      
+      try {
+         const result = await partLot.listParteLoteById();
+
+         res.status(200).json(result)
+      } catch (error) {
+
+         res.status(404).json('Erro')
+      }
+   });
+
+   // Lista todas as taxas de acordo com o parte lote selecionado
+   router.post('/listProcessesParteLoteById', async (req, res, next) => {
+      const { id } = req.body;
+
+      try {
+         const result = await partLot.listProcessesParteLoteById(id);
+
+         res.status(200).json(result)
+      } catch (error) {
+
+         res.status(404).json('Erro')
+      }
+   });
+
+   // Lista todas as taxas de acordo com o parte lote selecionado
+   router.post('/listAllRatesByParteLote', async (req, res, next) => {
+      const { IdLogistica_House, parte_lote_id } = req.body;
+
+      try {
+         const result = await partLot.listAllRatesByParteLote(IdLogistica_House, parte_lote_id);
+
+         res.status(200).json(result)
+      } catch (error) {
+
+         res.status(404).json('Erro')
+      }
+   });
+
    return router;
 }

2213d4b94565c7bb8d9d72376a7cf5c115ecc7a8
Ederson Arruda Ceccato <edersonceccato@gmail.com>
2025-01-06 15:43:53 -0300

    Situacao de embarque nao pode ser 4

--- server/controllers/cash-flow.js
+++ server/controllers/cash-flow.js
@@ -61,6 +61,7 @@ const cashFlow = {
             Lmo.IdMoeda = 110 -- BRL
             AND Lhs.Numero_Processo NOT LIKE '%test%'
             AND Lhs.Situacao_Agenciamento NOT IN (7)
+            AND Lms.Situacao_Embarque NOT IN (4)
             ${dateFilter}
       `)
       return result;

67beb497bea7a334a58618f795fe9a923dc29280
Petryck William <petryck@i9devgroup.com>
2025-02-27 10:35:58 -0300

    .
    .

--- server/routes/api.js
+++ server/routes/api.js
@@ -43,6 +43,7 @@ const apiMaterialControl = require('./api-material-control');
 const api_commercial_main = require('./api-commercial-main');
 const api_commercial_adm = require('./api-commercial-adm');
 const api_process_view = require('./api-process-view'); // Nova rota para visualização de processo
+const api_text_to_speech = require('./api-text-to-speech');
 
 const apiAppMonitor = require('./apiAppMonitor');
 const apiSystem = require('./api-system');
@@ -198,6 +199,9 @@ const setIO = (io) => {
 
   // Use as rotas do arquivo api-process-view.js
   router.use('/process-view', api_process_view(io)); // Nova rota para visualização de processo
+
+  // Use as rotas do arquivo api-text-to-speech.js
+  router.use('/text-to-speech', api_text_to_speech(io));
 
   return router;
 };

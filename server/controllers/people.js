284d881d1ce3e739f963abeed70c928bc44bf63d
Lucas Santos Peres <luc_peres@hotmail.com>
2025-09-19 17:13:12 -0300

    update /api-people

--- server/controllers/people.js
+++ server/controllers/people.js
@@ -29,6 +29,78 @@ const People = {
 
       return true;
    },
+   getById: async function(id){
+      let result = await executeQuery(`
+         SELECT
+            pl.name AS real_name,
+            pl.fantasy_name,
+            pl.type_people,
+            pl.cnpj_cpf,
+            cl.name,
+            cl.family_name,
+            pa.cep,
+            pa.address,
+            pa.complement,
+            ct.name AS city,
+            st.name AS state,
+            cn.name AS country,
+            pa.email,
+            pa.phone
+         FROM people pl
+         LEFT OUTER JOIN collaborators cl ON cl.id = pl.created_by
+         LEFT OUTER JOIN people_address pa ON pa.id_people = pl.id
+         LEFT OUTER JOIN city ct ON ct.id = pa.city
+         LEFT OUTER JOIN states st ON st.id = pa.state
+         LEFT OUTER JOIN country cn ON cn.id = pa.country
+         WHERE pl.id = ${id}`);
+
+      return result;
+   },
+   update: async function(details){
+
+      let result = await executeQuery(`
+         UPDATE people SET
+            name = '${details.realName}',
+            fantasy_name = '${details.fantasyName}',
+            cnpj_cpf = '${details.cpfCnpj}',
+            WHERE (id = '${details.id}');`);
+      
+      let address = await executeQuery(`
+         SELECT * FROM people_address
+         WHERE people_id = '${details.id}'`);
+      if (address) {
+         
+      }
+
+      return result;
+   },
+   getCities: async function(search){
+
+      let result = await executeQuery(`
+         SELECT *
+         FROM city
+         WHERE name like '%${search}%';`);
+
+      return result;
+   },
+   getStates: async function(search){
+
+      let result = await executeQuery(`
+         SELECT *
+         FROM states
+         WHERE name like '%${search}%';`);
+
+      return result;
+   },
+   getCountries: async function(search){
+
+      let result = await executeQuery(`
+         SELECT *
+         FROM country
+         WHERE name like '%${search}%';`);
+
+      return result;
+   },
 }
 
 module.exports = {
--- server/routes/api-people.js
+++ server/routes/api-people.js
@@ -5,7 +5,6 @@ const fs = require('fs');
 const { People } = require('../controllers/people');
 
 module.exports = function(io) {
-    // Lista todas as pessoas;
     router.get('/getAllPeople', async (req, res, next) => {
         try {
             const result = await People.getAllPeople();
@@ -17,11 +16,65 @@ module.exports = function(io) {
         }
     });
 
-    // Cria pessoa
     router.post('/insertPeople', async (req, res, next) => {
         try {
             const result = await People.insertPeople(req.body);
             io.emit('insertPeople', result);
+            res.status(200).json(result)
+        } catch (error) {
+            console.log(error);
+
+            res.status(404).json('Erro')
+        }
+    });
+
+    router.post('/getById', async (req, res, next) => {
+        try {
+            const result = await People.getById(req.body.id);
+            res.status(200).json(result)
+        } catch (error) {
+            console.log(error);
+
+            res.status(404).json('Erro')
+        }
+    });
+
+    router.post('/update', async (req, res, next) => {
+        try {
+            const result = await People.update(req.body);
+            res.status(200).json(result)
+        } catch (error) {
+            console.log(error);
+
+            res.status(404).json('Erro')
+        }
+    });
+
+    router.post('/getCities', async (req, res, next) => {
+        try {
+            const result = await People.getCities(req.body.search);
+            res.status(200).json(result)
+        } catch (error) {
+            console.log(error);
+
+            res.status(404).json('Erro')
+        }
+    });
+
+    router.post('/getStates', async (req, res, next) => {
+        try {
+            const result = await People.getStates(req.body.search);
+            res.status(200).json(result)
+        } catch (error) {
+            console.log(error);
+
+            res.status(404).json('Erro')
+        }
+    });
+
+    router.post('/getCountries', async (req, res, next) => {
+        try {
+            const result = await People.getCountries(req.body.search);
             res.status(200).json(result)
         } catch (error) {
             console.log(error);
